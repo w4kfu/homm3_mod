e005ac4c2d73e42630e7b160b2bd86e4f1aa6d02
Samuel CHEVET <gw4kfu@gmail.com>
2012-01-21 17:30:32 +0100

    Adding launcher for game and cd patch

--- main.cpp
+++ main.cpp
@@ -4,9 +4,76 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include "common.h"
 
-int main(int argc, char **argv)
+void	error(char *func_name)
 {
+	printf("[-] %s() failed, LastError = %x\n", func_name, GetLastError());
+	exit(EXIT_FAILURE);
+}
+
+int			setup_nocd(PROCESS_INFORMATION *pi, DWORD ImageBase)
+{
+	char	buf[5];
+	SIZE_T	written = 0;
+	DWORD	oldprot;
+
+	memset(buf, 0x90, 5);
+	VirtualProtect((LPVOID)(ImageBase + 0x10c2f4), 5, PAGE_EXECUTE_READWRITE, &oldprot);
+	if (!WriteProcessMemory(pi->hProcess, (LPVOID)(ImageBase + 0x10c2f4), buf, 5, &written) && written != 5)
+		error("WriteProcessMemory");
+	buf[0] = 0xEB;
+	VirtualProtect((LPVOID)(ImageBase + 0xed9df), 5, PAGE_EXECUTE_READWRITE, &oldprot);
+	if (!WriteProcessMemory(pi->hProcess, (LPVOID)(ImageBase + 0xed9df), buf, 1, &written) && written != 1)
+		error("WriteProcessMemory");
+	return (0);
+}
+
+int		launch_heroes3(void)
+{
+	STARTUPINFO					si;
+	PROCESS_INFORMATION			pi;
+	char						path_lsass[260];
+	PROCESS_BASIC_INFORMATION	pbi;
+	DWORD						ImageBase;
+	DWORD						read;
+
+	memset(&si, 0, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	memset(&pi, 0, sizeof(PROCESS_INFORMATION));
+	memset(&pbi, 0, sizeof(PROCESS_BASIC_INFORMATION));
+	if (!CreateProcess(L"HEROES3.EXE", NULL, NULL, NULL, NULL,
+					CREATE_SUSPENDED | DETACHED_PROCESS | CREATE_NO_WINDOW,
+					NULL, NULL, &si, &pi))
+		error("CreateProcess");
+	if (ZwQueryInformationProcess(pi.hProcess, 0, &pbi, sizeof(PROCESS_BASIC_INFORMATION), NULL) != 0)
+		error("ZwQueryInformationProcess");
+	if (!ReadProcessMemory(pi.hProcess, (BYTE*)pbi.PebBaseAddress + 8, &ImageBase, 4, &read) && read != 4)
+		error("ReadProcessMemory");
+	setup_nocd(&pi, ImageBase);
+	ResumeThread(pi.hThread);
+	return (0);
+}
+
+int			main(int argc, char **argv)
+{
+	FILE	*fp;
+
+
+	fp = fopen("HEROES3.EXE", "rb");
+	if (fp == NULL)
+	{
+		MessageBoxA(NULL, "Can't find HEROES3.EXE", "Error", MB_ICONERROR);
+		return (-1);
+	}
+	else
+	{
+		ZwQueryInformationProcess = (long (__stdcall *)(HANDLE, PROCESSINFOCLASS, PVOID, ULONG, PULONG))GetProcAddress(GetModuleHandleA("ntdll"),"ZwQueryInformationProcess");
+		if (!ZwQueryInformationProcess)
+			error("GetProcAddress");
+		launch_heroes3();
+	}
 
 	return (0);
 }
