0349918ebd6f73db9fa04b496ac6bf016c894fe6
Samuel CHEVET <gw4kfu@gmail.com>
2012-01-21 16:39:05 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* This program is just for fun and lulz
+	it try to add mofication to heroes of
+	might and magic 3 Complete Edition */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
